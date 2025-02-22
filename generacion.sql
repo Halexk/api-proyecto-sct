1050e2ccc36822d477ff4aa0113c9fe3005026c0
Hakel Obelmejias <hakel122003@gmail.com>
2025-02-22 19:26:57 -0400

    se acomodo el prompt para crear la base de datos

--- generacion.sql
+++ generacion.sql
@@ -5,9 +5,9 @@ USE equipment_db;
 -- Crear la tabla 'equipments'
 CREATE TABLE IF NOT EXISTS equipments (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    bienNacional VARCHAR(255) NOT NULL UNIQUE, -- Clave única
+    bienNacional VARCHAR(255) NOT NULL UNIQUE,
     tipoEquipo VARCHAR(255) NOT NULL,
-    numeroSerie INT NOT NULL,
+    numeroSerie VARCHAR(255) NOT NULL, -- Cambiado a VARCHAR
     estado ENUM('en uso', 'reparacion', 'inoperativo') NOT NULL,
     ubicacion VARCHAR(255) NOT NULL,
     asignacion VARCHAR(255) NOT NULL,
@@ -18,26 +18,28 @@ CREATE TABLE IF NOT EXISTS equipments (
 CREATE TABLE IF NOT EXISTS reports (
     id INT AUTO_INCREMENT PRIMARY KEY,
     equipment_id INT NOT NULL,
+    user_id INT NOT NULL,
     estado_anterior ENUM('en uso', 'reparacion', 'inoperativo') NOT NULL,
     estado_nuevo ENUM('en uso', 'reparacion', 'inoperativo') NOT NULL,
     ubicacion_anterior VARCHAR(255) NOT NULL,
     ubicacion_nueva VARCHAR(255) NOT NULL,
     asignacion_anterior VARCHAR(255) NOT NULL,
     asignacion_nueva VARCHAR(255) NOT NULL,
-    motivo ENUM('reubicacion', 'reparacion', 'reasignacion') NOT NULL,
+    motivo ENUM('reubicacion', 'reparacion', 'reasignacion', 'entrega') NOT NULL,
     observacion TEXT NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (equipment_id) REFERENCES equipments(id) ON DELETE CASCADE
-    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE;
+    FOREIGN KEY (equipment_id) REFERENCES equipments(id) ON DELETE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
 
+-- Crearla base de datos de los usuarios 
 CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(255) NOT NULL,
     apellido VARCHAR(255) NOT NULL,
-    dni VARCHAR(20) NOT NULL UNIQUE, -- DNI único
+    dni VARCHAR(20) NOT NULL UNIQUE,
     cargo VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL, -- Contraseña cifrada
+    password VARCHAR(255) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
